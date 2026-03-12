cd40c93f4d3f19b2f575376bae2cfa90926f5c23
PrasannaG7 <kalamprasanna7@gmail.com>
2026-03-12 11:58:46 +0530

    add the controllers

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,19 +1,33 @@
-const service = require("../services/mainService")
+const service = require('../services/mainService')
 
 exports.create = (req, res) => {
-    if (!req.body.title || !req.body.secret) {
-        return res.status(400).json({ message: "Missing required fields: title and secret" })
-    }
-    const item = service.create(req.body)
-    res.json(item)
+   try {
+      const item = service.create(req.body)
+      const safeItem = {
+         id: item.id,
+         title: item.title
+      }
+      res.status(201).json(safeItem)
+   } catch (err) {
+      res.status(500).json({ error: err.message })
+   }
 }
 
 exports.list = (req, res) => {
-    const data = service.list()
-    res.json(data)
+   try {
+      const data = service.list()
+      const safeData = data.map(item => ({ id: item.id, title: item.title }))
+      res.json(safeData)
+   } catch (err) {
+      res.status(500).json({ error: err.message })
+   }
 }
 
 exports.remove = (req, res) => {
-    service.remove(req.params.id)
-    res.json({ message: "deleted" })
+   try {
+      service.remove(req.params.id)
+      res.status(204).end()
+   } catch (err) {
+      res.status(500).json({ error: err.message })
+   }
 }
